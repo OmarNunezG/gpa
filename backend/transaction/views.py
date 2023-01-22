fc5c8d2b5920df3bbdfcbd3069ecbc187bbc151d
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-22 14:50:03 -0400

    Added transfer view (empty)

--- backend/transaction/views.py
+++ backend/transaction/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
+@api_view(["POST"])
+def transfer(self):
+    pass
