ade73eb2adea99856b15d71edb987437f237c7fb
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-22 23:03:09 -0400

    Added Transaction tests

--- backend/transaction/tests/test_transaction_api.py
+++ backend/transaction/tests/test_transaction_api.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 from core.models import Account
+from transaction.serializers import TransactionSerializer
+from user.serializers import UserSerializer
 
 AUTH_USER_URL = reverse("user:auth")
 CREATE_ACCOUNT_URL = reverse("account:create")
@@ -12,10 +14,11 @@ TRANSFER_URL = reverse("transaction:transfer")
 
 
 class TransactionTests(TestCase):
-    def setUp(self) -> None:
-        self.client = APIClient()
+    def get_user(self, username):
+        user = get_user_model().objects.get(username=username)
+        return user
 
-    def test_debit_transfer_success(self):
+    def set_up_user(self):
         register_user_payload = {
             "first_name": "user",
             "last_name": "Test",
@@ -30,21 +33,21 @@ class TransactionTests(TestCase):
         }
 
         account_payload = {
-            "username": "testuser",
+            "username": "usertest",
             "current_balance": 2000,
         }
 
         self.client.post(REGISTER_USER_URL, register_user_payload)
         self.client.post(AUTH_USER_URL, user_auth_payload)
-        res = self.client.post(CREATE_ACCOUNT_URL, account_payload)
+        self.client.post(CREATE_ACCOUNT_URL, account_payload)
+        self.user = self.get_user(register_user_payload["username"])
 
-        user = get_user_model().objects.get(
-            username=register_user_payload["username"]
-        )
-        for i in Account.objects.all():
-            print(i)
-        account = Account.objects.filter(user=user)
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.set_up_user()
 
+    def test_debit_transfer_success(self):
+        account = Account.objects.get(user=self.user)
         transfer_payload = {
             "transaction_type": "DEBIT",
             "note": "Store",
@@ -53,7 +56,24 @@ class TransactionTests(TestCase):
         }
 
         res = self.client.post(TRANSFER_URL, transfer_payload)
+        account = Account.objects.get(user=self.user)
 
-        self.assertEqual(res.status_code, status=status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual("success", res.data["status"])
         self.assertEqual(account.current_balance, 1990)
+
+    def test_credit_transfer_success(self):
+        account = Account.objects.get(user=self.user)
+        transfer_payload = {
+            "transaction_type": "CREDIT",
+            "note": "Mom",
+            "amount": 50,
+            "account": account.account_number,
+        }
+
+        res = self.client.post(TRANSFER_URL, transfer_payload)
+        account = Account.objects.get(user=self.user)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual("success", res.data["status"])
+        self.assertEqual(account.current_balance, 2050)
