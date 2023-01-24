64b8b617a0fff69c087c91556f9822275b1c108d
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-24 17:38:15 -0400

    Changed route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,14 +10,11 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginScreen />} />
           <Route path="/accounts" element={<AccountsScreen />} />
-          <Route path="/transactions" element={<TransactionsScreen />}>
-            <Route path="" element={<TransactionsScreen />} />
-            <Route path=":accountNumber" element={<TransactionsScreen />} />
-          </Route>
-          {/* <Route
+          <Route path="/transactions" element={<TransactionsScreen />} />
+          <Route
             path="/transactions/:accountNumber"
             element={<TransactionsScreen />}
-          /> */}
+          />
         </Routes>
       </main>
     </Router>
