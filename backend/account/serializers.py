a465e980bbb3f6d67343c6f41b1bbcecbe58b184
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-24 14:07:07 -0400

    Current balance field of account serializer is now float type

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -3,6 +3,12 @@ from core.models import Account
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    current_balance = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Account
         fields = ["account_number", "current_balance"]
+
+    def get_current_balance(self, obj: Account):
+        current_balance = float(obj.current_balance)
+        return current_balance
