9896808b196355bc53242019f942ae785f95ebc4
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-24 12:20:01 -0400

    Added navbar

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { List, ListItem } from '@mui/material';
+
+function NavBar() {
+  const { access } = JSON.parse(localStorage.getItem('userInfo'));
+  return (
+    <nav>
+      <h1>Welcome, {access}!</h1>
+      <List>
+        <ListItem>
+          <Link to="/accounts" underline="hover">
+            Accounts
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/transactions" underline="hover">
+            Transactions
+          </Link>
+        </ListItem>
+      </List>
+    </nav>
+  );
+}
+
+export default NavBar;
